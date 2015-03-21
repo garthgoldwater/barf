fa4fe5ab44fd4238fe329b15da2bec2cc49db828
Garth Goldwater <garth@Garths-MacBook-Pro.local>
2015-03-21 15:21:28 -0400

    add actions and make new messages make action & take actionId

--- messages/client/message_new.js
+++ messages/client/message_new.js
@@ -1,18 +1,36 @@
 Template.messageNew.events({
   "keypress .new-message": function(event, template) {
     if (event.which === 13) {
+      console.log("hello");
       var body = template.find(".new-body").value,
       barfId = this.barf._id;
 
       event.preventDefault();
 
+      var messageAddress = Messages.find({barfId: barfId}).count();
+      // this will not work for updating previous messages I think, and we
+      // still have to handle re-ordering messages after the fact
+      var newAction = _.extend({
+        type: "create",
+        noun: "message",
+        time: new Date(),
+        barfId: barfId,
+        nounId: null,
+        });
+
+      actionId = Actions.insert(newAction);
+
       var newMessage = _.extend({
         body: body,
         barfId: barfId,
-        userId: Meteor.userId()
+        userId: Meteor.userId(),
+        messageAddress: messageAddress,
+        actionId: actionId
       });
 
-      Messages.insert(newMessage);
+      messageId = Messages.insert(newMessage);
+
+      Actions.update({_id: actionId}, {$set: {nounId: messageId}})
 
       template.find(".new-body").value = "";
 
